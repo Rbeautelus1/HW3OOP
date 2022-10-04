0d83509a402860c3444e49212556a77a220b4782
Rbeautelus1 <103546272+Rbeautelus1@users.noreply.github.com>
2022-10-04 00:03:50 -0400

    Update CelestinalBody.cpp

--- CelestinalBody.cpp
+++ CelestinalBody.cpp
@@ -3,36 +3,47 @@ using namespace std;
 
 //add class definitions below this line
 
-class CelestialBody {
+class CelestialBody 
+{
   public:
-    CelestialBody(string n, double diam, double dist, int m) {
+    CelestialBody(string n, double diam, double dist, int m) 
+    {
       name = n;
       diameter = diam;
       distance = dist;
       moons = m;
     }
-    
+
     string name;
-    double diameter;
     double distance;
     int moons;
-};
+
+    double ComparedToEarth(){
+      double earth = 12756.3;
+    double relative_size = planet.diameter / earth;
+    return relative_size;
+    }
+    
+    private:
+   double diameter;
+    };
+
 
 //add class definitions above this line   
 
-double ComparedToEarth(CelestialBody planet) {
-  double earth = 12756.3;
-  double relative_size = planet.diameter / earth;
-  return relative_size;
-}
+
 
 int main() {
-  
+ 
   CelestialBody planet("Jupiter", 142984, 778360000, 79);
+  
+  
   
   //Modify the code below
   
-  cout << ComparedToEarth(planet) << endl;
+  cout << planet.ComparedToEarth() << endl;
+
+
 
   //Modify the code above
 
